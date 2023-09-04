101954e0b3decde65bb5a7bf1de9af41ecba6d31
wyldclaw <wyldclawthornbringer@gmail.com>
2023-09-04 16:52:20 -0700

    Update Eastern Kingdoms Cup

--- addons/BetterWorldMap_Dragonflight/zones/dragonriding.lua
+++ addons/BetterWorldMap_Dragonflight/zones/dragonriding.lua
@@ -23,12 +23,15 @@ local ugc = Map({id = 78, parent = 12}) -- Un'Goro Crater
 local uld = Map({id = 249, parent = 12}) -- Uldum
 local wsp = Map({id = 83, parent = 12}) -- Winterspring
 
+local bad = Map({id = 15, parent = 13}) -- Badlands
 local bla = Map({id = 17, parent = 13}) -- Blasted Lands
 local cos = Map({id = 210, parent = 13}) -- The Cape of Stranglethorn
 local dun = Map({id = 27, parent = 13}) -- Dun Morogh
 local dwp = Map({id = 42, parent = 13}) -- Deadwind Pass
 local elw = Map({id = 37, parent = 13}) -- Elwynn Forest
+local epl = Map({id = 23, parent = 13}) -- Eastern Plaguelands
 local lmd = Map({id = 48, parent = 13}) -- Lock Modan
+local rog = Map({id = 217, parent = 13}) -- Ruins of Gilneas
 local rrm = Map({id = 49, parent = 13}) -- Redridge Mountains
 local sea = Map({id = 32, parent = 13}) -- Searing Gorge
 local twi = Map({id = 241, parent = 13}) -- Twilight Highlands
@@ -74,21 +77,20 @@ local EasternKingdomsCup = Pin({
     group = ns.groups.EASTERN_KINGDOMS_CUP
 }) -- Eastern Kingdoms Cup
 
--- .pins[7571] = EasternKingdomsCup -- Gilneas Gambit
--- .pins[7580] = EasternKingdomsCup -- Plaguelands Plunge
--- .pins[7582] = EasternKingdomsCup -- Fuselight Night Flight
--- .pins[7583] = EasternKingdomsCup -- Krazzworks Klash
-
+bad.pins[7582] = EasternKingdomsCup -- Fuselight Night Flight
 bla.pins[7579] = EasternKingdomsCup -- Blasted Lands Bolt
 cos.pins[7577] = EasternKingdomsCup -- Gurubashi Gala
 cos.pins[7581] = EasternKingdomsCup -- Booty Bay Blast
 dun.pins[7578] = EasternKingdomsCup -- Ironforge Interceptor
 dwp.pins[7575] = EasternKingdomsCup -- Deadwind Derby
 elw.pins[7576] = EasternKingdomsCup -- Elwynn Forest Flash
+epl.pins[7580] = EasternKingdomsCup -- Plaguelands Plunge
 lmd.pins[7572] = EasternKingdomsCup -- Loch Modan Loop
+rog.pins[7571] = EasternKingdomsCup -- Gilneas Gambit
 rrm.pins[7584] = EasternKingdomsCup -- Redridge Rally
 sea.pins[7573] = EasternKingdomsCup -- Searing Slalom
 twi.pins[7574] = EasternKingdomsCup -- Twilight Terror
+twi.pins[7583] = EasternKingdomsCup -- Krazzworks Klash
 
 -------------------------------------------------------------------------------
 --------------------------------- Outland Cup ---------------------------------
