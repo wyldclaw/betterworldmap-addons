8015130453b0d994c1568f05e2d0e9308881734c
wyldclaw <wyldclawthornbringer@gmail.com>
2023-11-04 10:06:33 -0700

    Add upcoming cups

--- addons/BetterWorldMap_Dragonflight/zones/dragonriding.lua
+++ addons/BetterWorldMap_Dragonflight/zones/dragonriding.lua
@@ -39,6 +39,21 @@ local rrm = Map({id = 49, parent = 13}) -- Redridge Mountains
 local sea = Map({id = 32, parent = 13}) -- Searing Gorge
 local twi = Map({id = 241, parent = 13}) -- Twilight Highlands
 
+-- Outland
+local bem = Map({id = 105, parent = 101}) -- Blade's Edge Mountains
+local hel = Map({id = 100, parent = 101}) -- Hellfire Peninsula
+local nag = Map({id = 107, parent = 101}) -- Nagrand
+local net = Map({id = 109, parent = 101}) -- Netherstorm
+local smv = Map({id = 104, parent = 101}) -- Shadowmoon Valley
+local ter = Map({id = 108, parent = 101}) -- Terokkar Forest
+local zan = Map({id = 102, parent = 101}) -- Zangarmarsh
+
+-- Northrend
+
+-- Pandaria
+
+-- Broken Isles
+
 -------------------------------------------------------------------------------
 -------------------------------- KALIMDOR CUP ---------------------------------
 -------------------------------------------------------------------------------
@@ -82,3 +97,41 @@ rrm.pins[7584] = EasternKingdomsCup -- Redridge Rally
 sea.pins[7573] = EasternKingdomsCup -- Searing Slalom
 twi.pins[7574] = EasternKingdomsCup -- Twilight Terror
 twi.pins[7583] = EasternKingdomsCup -- Krazzworks Klash
+
+-------------------------------------------------------------------------------
+--------------------------------- OUTLAND CUP ---------------------------------
+-------------------------------------------------------------------------------
+
+local OutlandCup = Pin({group = ns.groups.OUTLAND_CUP}) -- Outland Cup
+
+bem.pins[7591] = OutlandCup -- Blade's Edge Brawl
+hel.pins[7589] = OutlandCup -- Hellfire Hustle
+hel.pins[7593] = OutlandCup -- Razorthorn Rise Rush
+nag.pins[7592] = OutlandCup -- Telaar Tear
+nag.pins[7599] = OutlandCup -- Warmaul Wingding
+net.pins[7595] = OutlandCup -- Tempest Keep Sweep
+net.pins[7598] = OutlandCup -- Eco-Dome Excursion
+smv.pins[7597] = OutlandCup -- Shadowmoon Slam
+smv.pins[7601] = OutlandCup -- Fel Pit Fracas
+ter.pins[7594] = OutlandCup -- Auchindoun Coaster
+ter.pins[7596] = OutlandCup -- Shattrath City Sashay
+ter.pins[7600] = OutlandCup -- Skettis Scramble
+zan.pins[7590] = OutlandCup -- Coilfang Caper
+
+-------------------------------------------------------------------------------
+-------------------------------- NORTHREND CUP --------------------------------
+-------------------------------------------------------------------------------
+
+local NorthrendCup = Pin({group = ns.groups.NORTHREND_CUP}) -- Northrend Cup
+
+-------------------------------------------------------------------------------
+-------------------------------- PANDARIA CUP ---------------------------------
+-------------------------------------------------------------------------------
+
+local PandariaCup = Pin({group = ns.groups.PANDARIA_CUP}) -- Pandaria Cup
+
+-------------------------------------------------------------------------------
+------------------------------ BROKEN ISLES CUP -------------------------------
+-------------------------------------------------------------------------------
+
+local BrokenIslesCup = Pin({group = ns.groups.BROKEN_ISLES_CUP}) -- Broken Isles Cup
